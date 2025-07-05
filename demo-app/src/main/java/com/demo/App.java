8375fdf136c7e606366a931288d96b1d9b130456
Rishav1212 <rishavchakraborty02@gmail.com>
2025-07-06 01:44:28 +0530

    Testing Jenkins trigger

--- demo-app/src/main/java/com/demo/App.java
+++ demo-app/src/main/java/com/demo/App.java
@@ -2,7 +2,7 @@ package com.demo;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI Pipeline!");
+        System.out.println("Triggered CI via GitHub push!");
     }
 }
 
